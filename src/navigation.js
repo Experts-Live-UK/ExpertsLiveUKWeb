760ca0cb978195adae013ef3b9948acfb4ee4382
nicholaschangIT <nicholasc001@hotmail.com>
2024-04-10 00:20:07 +0100

    wip website

--- src/navigation.js
+++ src/navigation.js
@@ -3,28 +3,16 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Speaker',
       links: [
         {
           text: 'SaaS',
           href: getPermalink('/homes/saas'),
         },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
       ],
     },
     {
-      text: 'Pages',
+      text: 'Agenda',
       links: [
         {
           text: 'Features (Anchor Link)',
@@ -34,30 +22,10 @@ export const headerData = {
           text: 'Services',
           href: getPermalink('/services'),
         },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
       ],
     },
     {
-      text: 'Landing',
+      text: 'Sponsors',
       links: [
         {
           text: 'Lead Generation',
@@ -67,26 +35,10 @@ export const headerData = {
           text: 'Long-form Sales',
           href: getPermalink('/landing/sales'),
         },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
       ],
     },
     {
-      text: 'Blog',
+      text: 'Events',
       links: [
         {
           text: 'Blog List',
@@ -96,23 +48,7 @@ export const headerData = {
           text: 'Article',
           href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
         },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
       ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
     },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
