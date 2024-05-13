d2814971a968079643fd7b3f1bc63745346c3551
unknown <joeyk2k@gmail.com>
2024-05-13 17:49:24 +0900

    homepage and footer

--- src/navigation.js
+++ src/navigation.js
@@ -74,47 +74,59 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'User Group',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        {
+          text: 'About the user group',
+          href: getPermalink('/usergroup'),
+        },
+        {
+          text: 'Planned events',
+          href: getPermalink('/usergroup/#planned'),
+        },
+        {
+          text: 'Previous events',
+          href: getPermalink('/usergroup/#previous'),
+        },
+        {
+          text: 'Call for Speakers',
+          href: getPermalink('/usergroupspeakers'),
+        },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Conference',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        {
+          text: 'The Conference',
+          href: getPermalink('/conference'),
+        },
       ],
     },
     {
-      title: 'Support',
+      title: 'Sponsors',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        {
+          text: 'The Sponsors',
+          href: getPermalink('/sponsors'),
+        },
       ],
     },
     {
-      title: 'Company',
+      title: 'More',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        {
+          text: 'About us',
+          href: getPermalink('/about'),
+        },
+        {
+          text: 'Contact',
+          href: getPermalink('/contact'),
+        },
+        {
+          text: 'Privacy policy',
+          href: getPermalink('/privacy'),
+        },
       ],
     },
   ],
@@ -130,7 +142,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 rounded-sm"></span>
+    Experts Live · All rights reserved 2024.
   `,
 };
